21a73747746ed9218d4345ebd2c8d671978f9e94
Jason Weden <message1@email.com>
2011-08-17 21:35:21 -0400

    Updated Text

--- src/main/scala/weden/jason/scalatest/repo/MainServlet.scala
+++ src/main/scala/weden/jason/scalatest/repo/MainServlet.scala
@@ -5,8 +5,16 @@ import javax.servlet._
 
 class MainServlet extends HttpServlet {
 	  override def doGet(request: HttpServletRequest, response: HttpServletResponse) = {
-		  response.setContentType("text/plain")
+		 // response.setContentType("text/plain")
 		  response.getWriter().println( exampleContent )  	  
 	  } 
-	  def exampleContent(): String = { "Hello world, from a Scala servlet!" }
+	  def exampleContent(): String = { """<html><h1>
+This looks like just a webpage but behind the scenes is something pretty cool.  
+This is a scala servlet that is deployed on the Google App Engine.  
+When time permits, I hope to build this out into something more than a 
+simple webpage being served up.  
+<p>The code here resides in my <a href="https://github.com/jweden/scalatest-repo">GitHub Repository</a>
+<p>
+My LinkedIn profile is <a href="http://www.linkedin.com/in/jasonweden">here</a>.
+</h1></html>""" }
 }
