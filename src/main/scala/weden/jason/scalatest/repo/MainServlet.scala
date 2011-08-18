f28de6483e3dfa7300438f78879b2419e4c35173
Jason Weden <message1@email.com>
2011-08-17 21:45:29 -0400

    Updated Text

--- src/main/scala/weden/jason/scalatest/repo/MainServlet.scala
+++ src/main/scala/weden/jason/scalatest/repo/MainServlet.scala
@@ -10,10 +10,10 @@ class MainServlet extends HttpServlet {
 	  } 
 	  def exampleContent(): String = { """<html><h1>
 This looks like just a webpage but behind the scenes is something pretty cool.  
-This is a scala servlet that is deployed on the Google App Engine.  
+This is a Scala servlet that is deployed on the Google App Engine.  
 When time permits, I hope to build this out into something more than a 
 simple webpage being served up.  
-<p>The code here resides in my <a href="https://github.com/jweden/scalatest-repo">GitHub Repository</a>
+<p>The source code here resides in my <a href="https://github.com/jweden/scalatest-repo">GitHub Repository</a>
 <p>
 My LinkedIn profile is <a href="http://www.linkedin.com/in/jasonweden">here</a>.
 </h1></html>""" }
